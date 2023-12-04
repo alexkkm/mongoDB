156df8c4d5afb284f046f862b7411a49e9ae088d
alexkkm <alexkong0222@gmail.com>
2023-12-04 16:31:26 +0800

    trying mongodb react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await fetch(
+      'http://localhost:5000/register', {
+      method: "post",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <h1>This is React WebApp </h1>
+      <form action="">
+        <input type="text" placeholder="name"
+          value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder="email"
+          value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit"
+          onClick={handleOnSubmit}>submit</button>
+      </form>
+
+    </>
   );
 }
 
