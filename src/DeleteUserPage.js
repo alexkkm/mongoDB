2c8332c81e219417c1da02513538e5440e756967
alexkkm <alexkong0222@gmail.com>
2024-01-25 16:02:01 +0800

    done for deletePage

--- src/DeleteUserPage.js
+++ src/DeleteUserPage.js
@@ -7,30 +7,31 @@ function DeleteUserPage() {
     const [deleteName, setDeleteName] = useState('');
     const [deleteEmail, setDeleteEmail] = useState('');
 
+    // Function for "searchButton" button
     const handleOnSearch = async (e) => {
         e.preventDefault();
 
-        //TODO: Optimize the catch then logic, remove result flag
-        var result;
-        // search for the user in the database
-        const response = await searchUserAPI('http://localhost:5000/searchUser/' + query).catch((error) => {
-            console.log(error)
-        }).then(result = true, result = false)
-
-        if (result) {
+        try {
+            // search for the user in the database
+            const response = await searchUserAPI('http://localhost:5000/searchUser/' + query);
             console.log("Successfully get data from URL");
+
             // if successfully get the data, save the response as "content"
-            setContent(response)
-            // update the "deleteName" by the serching response
-            setDeleteName(response[0].name)
-            // update the "deleteName" by the serching response
-            setDeleteEmail(response[0].email)
+            setContent(response);
+
+            // update the "deleteName" and "deleteEmail" with the first item from the response
+            if (response.length > 0) {
+                // update the "deleteName" by the serching response
+                setDeleteName(response[0].name);
+                // update the "deleteEmail" by the serching response
+                setDeleteEmail(response[0].email);
+            }
+        } catch (error) {
+            console.log(error);
         }
-    }
+    };
 
-
-
-
+    // Function for "deleteButton" button
     const handleOnDelete = async (e) => {
         e.preventDefault();
         let response = await fetch(
@@ -54,14 +55,14 @@ function DeleteUserPage() {
             <form action="">
                 <input type="text" placeholder="query"
                     value={query} onChange={(e) => setQuery(e.target.value)} />
-                <button type="submit"
+                <button type="submit" id="searchButton"
                     onClick={handleOnSearch}>submit</button>
             </form>
             <p>{JSON.stringify(content)}</p>
             <p>The user that you gonna delete:</p>
             <p>{deleteName}</p>
             <p>{deleteEmail}</p>
-            <button type="submit"
+            <button type="submit" id="deleteButton"
                 onClick={handleOnDelete}>delete</button>
         </div>
     );
